b8e6aff722667871b8073b920d0fb868abe3e8e1
unknown <agelos.kostas21@gmail.com>
2023-02-09 15:42:17 +0200

    backend template theme

--- backend/views/Product/graphs.php
+++ backend/views/Product/graphs.php
@@ -8,62 +8,85 @@ AppAsset::register($this);
 
 
 <body>
-<div class="py-5">
-    <div class="container">
-        <div class="row hidden-md-up">
-            <div class="row">
-                <div class="col-auto">
-                    <div class="card">
-                        <div class="card-block" style="width: 500px">
-                            <div id="barChart" style="width: 450px;height:450px;margin-left: 15px"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-auto">
-                    <div class="card">
-                        <div class="card-block">
-                            <div class="card-body p-4">
-                                <div class="col-auto">
-                                    <div class="d-flex">
-                                        <h5 class="flex-grow-1"><?php echo $data[0] ?></h5>
-                                        <h5><?php echo $data[1] ?></h5>
-                                    </div>
-                                </div>
-
-                                <div class="d-flex flex-column text-center mt-5 mb-4">
-                                    <h6 class="display-4 mb-0 font-weight-bold"
-                                        style="color: #1C2331;"> <?php echo $data[2] ?>°C </h6>
-                                    <span class="small" style="color: #868B94"><?php echo $data[3] ?></span>
-                                </div>
-
-                                <div class="d-flex align-items-center">
-                                    <div class="flex-grow-1" style="font-size: 1rem;">
-                                        <div><i class="fas fa-wind fa-fw" style="color: #868B94;"></i> <span
-                                                    class="ms-1"> <?php echo $data[4] ?>km/h
-                  </span></div>
-                                        <h6>Feels like: <b><?php echo $data[5]?>°C</b></h6>
-                                    </div>
-                                    <div>
-                                        <img src="<?php echo $image ?>"
-                                             width="60px">
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div class="row">
+    <div class="col">
+<div class="card">
+    <div id="barChart" style="width: 300px;height:300px;margin-left: 15px"></div>
+</div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <div id="barChart2" style="width: 530px;height:300px;margin-left: 15px"></div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <div id="barChart3" style="width: 300px;height:300px;margin-left: 15px"></div>
         </div>
     </div>
 </div>
+
+<!---->
+<!--<div class="py-5">-->
+<!--    <div class="container">-->
+<!--        <div class="row hidden-md-up">-->
+<!--            <div class="row">-->
+<!--                <div class="col-auto">-->
+<!--                    <div class="card">-->
+<!--                        <div class="card-block" style="width: 500px">-->
+<!--                            <div id="barChart" style="width: 450px;height:450px;margin-left: 15px"></div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="col-auto" style="margin-left: 30px">-->
+<!--                    <div class="card">-->
+<!--                        <div class="card-block" style="width: 500px">-->
+<!--                            <div class="card-body p-4">-->
+<!--                                <div class="col-auto">-->
+<!--                                    <div class="d-flex">-->
+<!--                                        <h5 class="flex-grow-1">--><?php //echo $data[0] ?><!--</h5>-->
+<!--                                        <h5>--><?php //echo $data[1] ?><!--</h5>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!---->
+<!--                                <div class="d-flex flex-column text-center mt-5 mb-4">-->
+<!--                                    <h6 class="display-4 mb-0 font-weight-bold"-->
+<!--                                        style="color: #1C2331;"> --><?php //echo $data[2] ?><!--°C </h6>-->
+<!--                                    <span class="small" style="color: #868B94">--><?php //echo $data[3] ?><!--</span>-->
+<!--                                </div>-->
+<!---->
+<!--                                <div class="d-flex align-items-center">-->
+<!--                                    <div class="flex-grow-1" style="font-size: 1rem;">-->
+<!--                                        <div><i class="fas fa-wind fa-fw" style="color: #868B94;"></i> <span-->
+<!--                                                    class="ms-1"> --><?php //echo $data[4] ?><!--km/h-->
+<!--                  </span></div>-->
+<!--                                        <h6>Feels like: <b>--><?php //echo $data[5]?><!--°C</b></h6>-->
+<!--                                    </div>-->
+<!--                                    <div>-->
+<!--                                        <img src="--><?php //echo $image ?><!--"-->
+<!--                                             width="60px">-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!---->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
 
 </body>
 
 <?php
 $script = <<< JS
 let barChart = echarts.init(document.getElementById('barChart'));
+let barChart2 = echarts.init(document.getElementById('barChart2'));
+let barChart3 = echarts.init(document.getElementById('barChart3'));
 barChart.setOption(makeBarChart($active,$non));
+barChart2.setOption(makeBlueChart());
+barChart3.setOption(makeBarChart($active,$non));
 JS;
 $this->registerJs($script);
 ?>
--- backend/web/js/main.js
+++ backend/web/js/main.js
@@ -9,11 +9,71 @@ document.getElementById("btnReset").onclick = function () {
     document.getElementById("productsearch-globalsearch").value = ""
 }
 
+function makeBlueChart(){
+    let base = +new Date(1988, 9, 3);
+    let oneDay = 24 * 3600 * 1000;
+    let data = [[base, Math.random() * 300]];
+    for (let i = 1; i < 20000; i++) {
+        let now = new Date((base += oneDay));
+        data.push([+now, Math.round((Math.random() - 0.5) * 20 + data[i - 1][1])]);
+    }
+   return option = {
+        tooltip: {
+            trigger: 'axis',
+            position: function (pt) {
+                return [pt[0], '10%'];
+            }
+        },
+        title: {
+            left: 'center',
+            text: 'Large Ara Chart'
+        },
+        toolbox: {
+            feature: {
+                dataZoom: {
+                    yAxisIndex: 'none'
+                },
+                restore: {},
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'time',
+            boundaryGap: false
+        },
+        yAxis: {
+            type: 'value',
+            boundaryGap: [0, '100%']
+        },
+        dataZoom: [
+            {
+                type: 'inside',
+                start: 0,
+                end: 20
+            },
+            {
+                start: 0,
+                end: 20
+            }
+        ],
+        series: [
+            {
+                name: 'Fake Data',
+                type: 'line',
+                smooth: true,
+                symbol: 'none',
+                areaStyle: {},
+                data: data
+            }
+        ]
+    };
+}
+
 function makeBarChart(active, non) {
     var colorPalette = ['#08ff00', '#ff001e'];
     return {
         title: {
-            text: 'Active and non-active products',
+            text: 'Product Status',
             left: 'center'
         },
         tooltip: {

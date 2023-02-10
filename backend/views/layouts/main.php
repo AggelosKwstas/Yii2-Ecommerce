24644529c62d7c93fb04f5ec5323bd6250993cd0
unknown <agelos.kostas21@gmail.com>
2023-02-10 16:31:05 +0200

    layout

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -88,7 +88,7 @@ AppAsset::register($this);
             <div id="content">
                 <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
+                    <div style="margin-left: auto; color: #31343a "><span style="font-weight: bolder; font-size: 25px;color: #31343a">GAIA Platform</span></div>
                     <!-- Sidebar Toggle (Topbar) -->
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                         <i class="fa fa-bars"></i>
@@ -97,30 +97,6 @@ AppAsset::register($this);
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
-
-                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a>
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                 aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small"
-                                               placeholder="Search for..." aria-label="Search"
-                                               aria-describedby="basic-addon2">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
 
                         <div clas="nav-item">
                             <p3 style="color: #31343a"><b>Follow us on:</b></p3>
@@ -162,7 +138,6 @@ AppAsset::register($this);
 <!--                                </a>-->
 <!--                            </div>-->
 <!--                        </li>-->
-
                     </ul>
 
                 </nav>
